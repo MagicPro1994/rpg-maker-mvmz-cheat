4f2c316805fe26ba03d238836f74282070b1b4dd
Jin <jin.guen@1bytesoft.com>
2023-10-28 15:36:53 +0700

    Prevent app to load when the game is not loaded.

--- src/layouts/default/AppView.vue
+++ src/layouts/default/AppView.vue
@@ -1,9 +1,18 @@
 <script setup>
-//
+import { storeToRefs } from "pinia";
+import { useAppStore } from "@/store/app";
+
+const appStore = useAppStore();
+const { gameMaster } = storeToRefs(appStore);
+const reloadPage = () => window.location.reload();
 </script>
 
 <template>
   <v-main>
-    <router-view />
+    <v-card-text v-if="!gameMaster.isInGame()">
+      Please start the game first. Then reload the page.
+      <v-btn variant="text" icon="mdi-refresh" @click.stop="reloadPage"></v-btn>
+    </v-card-text>
+    <router-view v-else />
   </v-main>
 </template>
